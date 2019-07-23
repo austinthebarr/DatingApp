3612e049e9b0d03c2ecf6faed709eb8e89032139
Austin Barr <austinbarr@protonmail.com>
2019-07-23 14:10:49 -0700

    intial commit

---
+++ DatingWebsite.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using DatingWebsite.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingWebsite.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+        public DbSet<Value> Values { get; set;}
+    }
+}
